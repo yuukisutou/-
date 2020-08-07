b26af03ac7f5f00b21e9954fcad6ddbc033760d6
須藤雄紀 <sudouyuuki@mba.local>
2020-08-07 14:57:37 +0900

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,69 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).page(params[:page]).per(20)
+
+    respond_to do |format|
+      format.html
+      format.csv {send_data @tasks.generate_csv, filename: "tasks-#{Time.zone.now.strftime('%Y%m%d%s')}.csv"}
+    end
+  end
+  
+  def import
+    current_user.tasks.import(params[:file])
+    redirect_to tasks_url, notice: "タスクを追加しました"
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+
+  def edit
+  end
+
+  def update
+    @task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました。"
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    if params[:back].present?
+      render :new
+      return
+    end
+
+    if @task.save
+      TaskMailer.creation_email(@task).deliver_now
+      SampleJob.perform_later
+      redirect_to @task, notice: "タスク「#{@task.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new unless @task.valid?
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :image)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'taskleaf@example.com'
+  layout 'mailer'
+end
